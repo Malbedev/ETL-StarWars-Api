f84d0ec9b682b29ce40efa65a08c4e0ba827ed1c
Mauro <mauroalberelli@gmail.com>
2024-06-01 11:25:42 -0300

    primer-commit

---
+++ sql_data.sql
@@ -0,0 +1,24 @@
+SHOW TABLES
+FROM
+SCHEMA "data-engineer-database"."mauroalberelli_coderhouse";
+
+DROP TABLE IF EXISTS mauroalberelli_coderhouse.stage_starwars_table;
+
+CREATE TABLE stage_starwars_table(
+    id  SERIAL PRIMARY KEY,
+	Name	        VARCHAR(200)
+,   Gender          VARCHAR(50)
+,   Birth_year      VARCHAR(50)
+,   Eye_color       VARCHAR(50)
+,   Skin_color      VARCHAR(50)
+,   Hair_color      VARCHAR(50)
+,   Mass            VARCHAR(50)
+,   Height          VARCHAR(50)
+,   Homeworld       VARCHAR(50)
+
+
+);
+
+SELECT  
+*
+FROM mauroalberelli_coderhouse.stage_starwars_table ;
